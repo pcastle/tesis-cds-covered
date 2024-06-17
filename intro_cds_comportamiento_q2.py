2854658d14531acf75e0a2dfb34ae88ae0c83c50
Patricio Castillo <pcastillod@fen.uchile.cl>
2024-06-17 02:12:50 -0400

    se actualiza

--- intro_cds_comportamiento_q2.py
+++ intro_cds_comportamiento_q2.py
@@ -135,101 +135,9 @@ def plotIssuedAmountCompCDS(b1_v,b2_v,p1_v,q1_v,r_v,g1,g2):
     # Emisor 2
     ## p*q > b barra 
     vf2_1 = integrate(Min(y2/q2,1)*g2,(y2,0,1))
-from matplotlib.colors import ListedColormap
-
-
-
-# Eequilibrio buscado
-b2_v = 0.4
-
-y2, p2, q2, b2, r = sympy.symbols('y2 p2 q2 b2 r', real=True, positive=True)
-t2 = sympy.symbols('t2', positive=True)
-
-g2 = 3*(1-t2)*(y2-1)**2 + 3*t2*y2**2
-
-# def plotIssuedAmount():
-## p*q > b barra 
-vf2_1 = integrate(Min(y2/q2,1)*g2,(y2,0,1))
-
-## p*q < b barra y p >= b barra
-vf2_2 = integrate(Min(p2*y2/b2,1)*g2,(y2,0,1))
-
-## p*q < b barra y p < b barra
-# No need since vf2_2 already includes this case
-
-
-
-
-# theta gorro
-## p*q > b barra 
-tgorro2_1 = solve(vf2_1 - p2/(p2+r),t2)[0]
-tgorro2_1n = solve(vf2_1 - p2,t2)[0]
-
-## p*q < b barra y p >= b barra
-tgorro2_2 = solve(vf2_2 - p2/(p2+r),t2)[0]
-tgorro2_2n = solve(vf2_2 - p2,t2)[0]
-
-## p*q < b barra y p < b barra
-# No need since tgorro2_2 and tgorro2_2n already includes this case
-
-# p = sympy.plot_implicit(1-tgorro2_1.subs([(r,0.4),(b2,0.3)])-p2*q2,(p2,0,1),(q2,0,1),depth = 3)
-# print(p[0].get_points())
-
-
-d2 = Piecewise((1 - r/(p2 +r)*tgorro2_2, (p2 + r <= 1) & (p2*q2 <= b2)),
-               (1 - r/(p2 +r)*tgorro2_1, (p2 + r <= 1) & (p2*q2 > b2)),
-               (1-tgorro2_2n,(p2 + r > 1) & (p2*q2 <= b2)),
-               (1-tgorro2_1n,True))
-# sympy.plot_implicit(d2.subs([(r,0.2),(b2,0.3)])-p2*q2,(p2,0,1),(q2,0,1))
-d2 = Max(0,Min(1,d2))
-
-b2_v, r_v = 0.3, 0.4
-d2_fun = sympy.lambdify([q2, p2], d2.subs([(b2,b2_v),(r,r_v)]))
-lin_space = np.linspace(0,1,100)
-p2_lin, q2_lin = np.meshgrid(lin_space,lin_space)
-res = np.zeros((100,100))
-
-for ii in range(len(lin_space)):
-    for jj in range(len(lin_space)):
-        res[ii,jj] = d2_fun(q2_lin[ii,jj],p2_lin[ii,jj])
-
-plt.figure()
-cmap = ListedColormap(["tab:blue", "tab:blue"])
-plt.contour(q2_lin, p2_lin, res, levels=[0], cmap=cmap)
-plt.show()
-
-
-# Eequilibrio buscado
-y1, p1, q1, b1, y2, p2, q2, b2, r = sympy.symbols('y1 p1 q1 b1 y2 p2 q2 b2 r', real=True, positive=True)
-t1, t2 = sympy.symbols('t1 t2', positive=True)
-
-# Eequilibrio buscado
-# b1_vec = [0.3, 0.3, 0.4, 0.3]
-# b2_vec = [0.3,0.4, 0.4, 0.55]
-b1_v = 0.3
-b2_v = 0.4
-h3 = 3*y2**2
-
-g1 = 3*(1-t1)*(y1-1)**2 + 3*t1*y1**2
-g2 = 3*(1-t2)*(y2-1)**2 + 3*t2*y2**2
-
-# Emisor 1
-
-vf1 = parse_expr((pycode(integrate(Min(p1*y1/b1,1)*g1,(y1,0,1)))).replace("min(1, b1/p1)","(b1/p1)"), locals())
-tgorro1 = solve(vf1 - p1/(p2+r),t1)[0]
-tgorro1_n = solve(vf1 - p1,t1)[0]
-
-
-
-# Emisor 2
-## p*q > b barra 
-vf2_1 = integrate(Min(y2/q2,1)*g2,(y2,0,1))
 
     ## p*q < b barra y p >= b barra
     vf2_2 = integrate(Min(p2*y2/b2,1)*g2,(y2,0,1))
-
-## p*q < b barra y p < b barra
-vf2_3 = integrate(p2*y2/b2*g2,(y2,0,1))
 
     ## p*q < b barra y p < b barra
     vf2_3 = integrate(p2*y2/b2*g2,(y2,0,1))
@@ -391,5 +299,4 @@ if __name__ == '__main__':
 # ax.set(xlabel = 'precio $(p_2)$',
 #        ylabel = 'cantidad $(q_2)$')
 # plt.show()
-
 
